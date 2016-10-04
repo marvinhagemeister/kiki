3b12a44b5a215731d82befa77b0031baeb34c732
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-10-04 18:04:02 +0200

    Filter out sass partials when they have no parent

--- src/plugins/node-sass/getRootFiles.ts
+++ src/plugins/node-sass/getRootFiles.ts
@@ -7,10 +7,13 @@ export function getRootFiles(searchPath: string, modified: IFile): IFile[] {
   const graph = parseDir(searchPath);
 
   const node = graph.index[path.resolve(location)];
-  const files = typeof node !== "undefined"
+  let files = typeof node !== "undefined"
     && node.importedBy.length > 0
     ? node.importedBy
     : [location];
+
+  // Filter out partials which always start with "_"
+  files = files.filter(file => !path.basename(file).startsWith("_"));
 
   return files.map(file => {
     return Object.assign({}, modified, {

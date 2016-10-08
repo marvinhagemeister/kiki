7b70678c6765dbe329a8c9cc0c28544fefa3de45
Marvin Hagemeister <marvin.hagemeister@sevenval.com>
2016-10-08 17:44:26 +0200

    Add more tests

--- src/plugins/node-sass/__tests__/stream.spec.ts
+++ src/plugins/node-sass/__tests__/stream.spec.ts
@@ -1,10 +1,10 @@
-import ConsoleStream from "../../../ConsoleStream";
 import { IFile2 } from "../../../io/file";
 import MemoryStream from "../../../streams/memory-stream";
 import MemoryWriter from "../../../streams/memory-writer";
 import SassTransform from "../stream";
 import { Buffer } from "buffer";
 import { assert as t } from "chai";
+import "mocha";
 
 const errorHandler = (err: Error) => {
   console.log(err);
@@ -37,13 +37,20 @@ describe("NodeSass Stream", () => {
       .pipe(writer);
   });
 
-  it("should compile with sourcemaps", () => {
+  it("should compile with sourcemaps", done => {
     const stream = new MemoryStream();
     const sass = new SassTransform({ outputStyle: "compressed" });
     const writer = new MemoryWriter((file: IFile2) => {
-      t.equal(file.content.toString(), "body{color:red}\n");
+      t.equal(file.content.toString(), "body{color:red}\n\n/*# sourceMappingURL=whatever.css.map */");
       t.equal(file.location, "/tmp/whatever.css");
-      t.equal(file.map.toString(), "trueasf");
+
+      const map = JSON.parse(file.map.toString());
+      t.equal(map.version, 3);
+      t.equal(map.file, "whatever.css");
+      t.equal(map.sourceRoot, "/tmp");
+      t.equal(map.sources.length, 1);
+      t.isTrue(map.mappings.length > 1);
+      done();
     });
 
     stream.on("error", errorHandler);

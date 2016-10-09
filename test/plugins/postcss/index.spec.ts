46f9d369f0ac30465bcdb9e676c77726c432ba2e
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-10-09 18:34:16 +0200

    Remove unused files

--- test/plugins/postcss/index.spec.ts
+++
@@ -1,9 +0,0 @@
-// import * as postcss from "../../../src/plugins/postcss/index";
-// import { assert as t } from "chai";
-// import "mocha";
-
-// describe("index (postccss)", () => {
-//   it("should provide exports", () => {
-//     t.equal(typeof postcss.compile, "function");
-//   });
-// });

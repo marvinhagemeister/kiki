23bf80f1f6803f0044db8abaf520be3465c7c9b3
Marvin Hagemeister <marvin.hagemeister@sevenval.com>
2016-10-25 15:22:22 +0200

    Add option to force production builds

--- src/cli/watch.ts
+++ src/cli/watch.ts
@@ -3,6 +3,11 @@ import { build } from "../tasks/build";
 import { watch } from "../tasks/watch";
 
 const configPath = process.argv[2];
+const forceProduction = process.argv[3];
+
+if (forceProduction) {
+  process.env.NODE_ENV = "production";
+}
 
 const config = getConfig(configPath);
 build(config)

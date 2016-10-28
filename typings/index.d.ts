66d91c2c1cafe30ed70740444fe52d512dec1d8d
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-10-28 21:48:22 +0200

    Cleanup glob typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,5 @@
 /// <reference path="autoprefixer/index.d.ts" />
 /// <reference path="cssnano/index.d.ts" />
-/// <reference path="glob-promise/index.d.ts" />
 /// <reference path="globals/autoprefixer-core/index.d.ts" />
 /// <reference path="postcss-cssnext/index.d.ts" />
 /// <reference path="sass-graph/index.d.ts" />

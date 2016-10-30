9b7b1c4e682835ab39918867eea2ae061bb542a1
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-10-30 09:02:16 +0100

    Test kiki exports

---
+++ src/__tests__/index.spec.ts
@@ -0,0 +1,13 @@
+import * as index from "../index";
+import { assert as t } from "chai";
+import "mocha";
+
+describe("main index", () => {
+  it("should have correct exports", () => {
+    t.isFunction(index.getConfig);
+    t.isFunction(index.build);
+    t.isFunction(index.watch);
+
+    t.equal(Object.keys(index).length, 3);
+  });
+});
